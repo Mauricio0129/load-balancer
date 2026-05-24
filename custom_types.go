ac9052d94cd6f6993d917c2d286935ddc3dd18c6
Mauricio0129 <mauricioantonio1227@gmail.com>
2026-05-23 23:58:46 -0400

    Add configuration structure and main logic for loading config

--- custom_types.go
+++ custom_types.go
@@ -1,1 +1,14 @@
 package main
+
+type Config struct {
+	Host       string              `json:"host"`
+	Port       string              `json:"port"`
+	Backends   map[string][]string `json:"backends"`
+	Tls_config Tls                 `json:"tls"`
+}
+
+type Tls struct {
+	Enabled  bool   `json:"enabled"`
+	CertFile string `json:"certfile"`
+	KeyFile  string `json:"keyfile"`
+}
--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
+	config := &Config{}
+
+	data, err := os.ReadFile("config/config.json")
+	if err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+
+	err = json.Unmarshal(data, config)
+	if err != nil {
+		log.Fatalf("Error parsing config file: %v", err)
+	}
+
+	fmt.Printf("Loaded config: %+v\n", config)
 }
